b2d4592f6cdc3dabaa762a4f391e8878903d4c12
Joseph <joseph.a.curtis@gmail.com>
2020-09-26 20:43:22 -0700

    First Commit

---
+++ migrations/2-initial_migration.js
@@ -0,0 +1,141 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * removeColumn "id" from table "users"
+ * changeColumn "updatedAt" on table "users"
+ * changeColumn "createdAt" on table "users"
+ * changeColumn "Username" on table "users"
+ * changeColumn "Email" on table "users"
+ * changeColumn "UserId" on table "users"
+ * changeColumn "UserId" on table "users"
+ * changeColumn "UserId" on table "users"
+ *
+ **/
+
+var info = {
+    "revision": 2,
+    "name": "initial_migration",
+    "created": "2020-09-27T03:38:20.486Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+        fn: "removeColumn",
+        params: ["users", "id"]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "updatedAt",
+            {
+                "type": Sequelize.DATE,
+                "field": "updatedAt"
+            }
+        ]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "createdAt",
+            {
+                "type": Sequelize.DATE,
+                "field": "createdAt"
+            }
+        ]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "Username",
+            {
+                "type": Sequelize.STRING,
+                "field": "Username",
+                "unique": true
+            }
+        ]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "Email",
+            {
+                "type": Sequelize.STRING,
+                "field": "Email",
+                "unique": true
+            }
+        ]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "UserId",
+            {
+                "type": Sequelize.INTEGER,
+                "field": "UserId",
+                "primaryKey": true,
+                "autoIncrement": true,
+                "allowNull": false
+            }
+        ]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "UserId",
+            {
+                "type": Sequelize.INTEGER,
+                "field": "UserId",
+                "primaryKey": true,
+                "autoIncrement": true,
+                "allowNull": false
+            }
+        ]
+    },
+    {
+        fn: "changeColumn",
+        params: [
+            "users",
+            "UserId",
+            {
+                "type": Sequelize.INTEGER,
+                "field": "UserId",
+                "primaryKey": true,
+                "autoIncrement": true,
+                "allowNull": false
+            }
+        ]
+    }
+];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
